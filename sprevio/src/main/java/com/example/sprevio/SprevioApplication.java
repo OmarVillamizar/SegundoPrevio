27d6ddd4e37cf5b5828176c9def075da9f2eb888
Omar Villamizar <omarjesusvi@ufps.edu.co>
2024-05-26 23:03:03 -0500

    Segundo Previo - Omar Villamizar 1152239, inicializado con una plantilla básica de Spring con un HealthController.

---
+++ sprevio/src/main/java/com/example/sprevio/SprevioApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprevio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprevioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprevioApplication.class, args);
+	}
+
+}
---
+++ sprevio/src/test/java/com/example/sprevio/SprevioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprevio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprevioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
